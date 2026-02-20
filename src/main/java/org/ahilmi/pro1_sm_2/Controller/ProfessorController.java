9e4d2f4d3f513c0a3fc86211f98d94604fc99588
ahilmii <ahmet1905hilmi@gmail.com>
2026-02-20 20:20:33 +0300

    prof controller completed

--- src/main/java/org/ahilmi/pro1_sm_2/Controller/ProfessorController.java
+++ src/main/java/org/ahilmi/pro1_sm_2/Controller/ProfessorController.java
@@ -6,18 +6,23 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import org.ahilmi.pro1_sm_2.db.CourseCrudOperations;
 import org.ahilmi.pro1_sm_2.db.ProfessorCrudOperations;
+import org.ahilmi.pro1_sm_2.dto.Course;
 import org.ahilmi.pro1_sm_2.dto.Professor;
+
+import java.util.Optional;
 
 public class ProfessorController {
     @FXML
-    private TextField ProfessorName;
+    private TextField professorName;
 
     @FXML
-    private TextField ProfessorId;
+    private TextField professorId;
 
     @FXML
-    private TextField ProfessorDepartment;
+    private TextField professorDepartment;
+
     @FXML
     private Button updateProfessor;
 
@@ -42,28 +47,120 @@ public class ProfessorController {
             alert.setTitle("Error");
             alert.setHeaderText("Id is wrong!");
             alert.showAndWait();
-            clearPrefessor(event);
+            clearProfessor(event);
         }
     }
 
-    public void closePrefessor(ActionEvent event) {Platform.exit();}
+    public void closeProfessor(ActionEvent event) {Platform.exit();}
 
-    public void clearPrefessor(ActionEvent event) {
-        ProfessorId.setText("");
-        ProfessorName.setText("");
-        ProfessorDepartment.setText("");}
-    public void savePrefessor(ActionEvent event) {
-        checkId((ProfessorId.getText()), event);
+    public void clearProfessor(ActionEvent event) {
+        professorId.setText("");
+        professorName.setText("");
+        professorDepartment.setText("");}
+
+    public void saveProfessor(ActionEvent event) {
+        checkId(professorId.getText(), event);
         Professor professor = new Professor();
 
-    }
-    public void deletePrefessor(ActionEvent event) {
+        professor.setId(Integer.parseInt(professorId.getText()));
+        professor.setName(professorName.getText());
+        professor.setDepartment(professorDepartment.getText());
+
+        ProfessorCrudOperations crudOperations = new ProfessorCrudOperations();
+        int res = crudOperations.insertProfessorById(professor);
+
+        if(res > 0){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Success");
+            alert.setHeaderText("Professor with id " + professorId.getText() + " saved");
+            alert.showAndWait();
+            clearProfessor(event);
+        } else if(res == -1){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("There is a professor with the same id: " + professorId.getText());
+            alert.showAndWait();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Error on saving professor!");
+            alert.showAndWait();
+        }
 
     }
-    public void getPrefessor(ActionEvent event) {
+
+    public void deleteProfessor(ActionEvent event) {
+        checkId(professorId.getText(), event);
+        ProfessorCrudOperations crudOperations = new ProfessorCrudOperations();
+
+        int id = Integer.parseInt(professorId.getText());
+        int result = crudOperations.deleteProfessorById(id);
+
+        if(result > 0){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Success");
+            alert.setHeaderText("Professor with id " + professorId.getText() + " deleted");
+            alert.showAndWait();
+            clearProfessor(event);
+        } else if(result == -1){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("There isn't any professor with the id: " + professorId.getText());
+            alert.showAndWait();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Error on deleting professor!");
+            alert.showAndWait();
+        }
 
     }
-    public void updatePrefessor(ActionEvent event) {
+
+
+    public void getProfessor(ActionEvent event) {
+        checkId(professorId.getText(), event);
+        ProfessorCrudOperations crudOperations = new ProfessorCrudOperations();
+
+        int id = Integer.parseInt(professorId.getText());
+        Optional<Professor> professor = crudOperations.getProfessorById(id);
+
+        if(professor.isPresent()){
+            professorId.setText(Integer.toString(professor.get().getId()));
+            professorName.setText(professor.get().getName());
+            professorDepartment.setText(professor.get().getDepartment());
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Professor with the id " + id + " not found");
+            alert.showAndWait();
+        }
+    }
+
+
+
+    public void updateProfessor(ActionEvent event) {
+        checkId(professorId.getText(), event);
+        Professor professor = new Professor();
+
+        professor.setId(Integer.parseInt(professorId.getText()));
+        professor.setName(professorName.getText());
+        professor.setDepartment(professorDepartment.getText());
+
+        ProfessorCrudOperations crudOperations = new ProfessorCrudOperations();
+        int res = crudOperations.updateProfessorById(professor);
+
+        if(res > 0) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Success");
+            alert.setHeaderText("Professor with id " + professorId.getText() + " updated");
+            alert.showAndWait();
+            clearProfessor(event);
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Error on updating professor!");
+            alert.showAndWait();
+        }
 
     }
 
